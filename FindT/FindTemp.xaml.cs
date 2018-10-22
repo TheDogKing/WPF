e43c0bc227fec2655ea52f89df814076e87cccfc
Liao <dengfeng.liao@intergraph.com>
2018-10-22 19:45:07 +0800

    Add project files.

---
+++ FindT/FindTemp.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+
+namespace FindT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class FindTemp : Window
+    {
+        public Hashtable AnswerHashTable = new Hashtable();
+        public ArrayList HashtableList = new ArrayList();
+        public FindTemp()
+        {
+            InitializeComponent();
+            //ControlTemplate baseWindowTemplate = (ControlTemplate)App.Current.Resources["MyWindowTemplate"];
+        }
+
+        private void FindT_Click(object sender, RoutedEventArgs e)
+        {
+            string AnswerT = "Error";
+            try
+            {
+                double T1 = Convert.ToDouble(FT.Text.Trim());
+                double Dc = Convert.ToDouble(DC.Text.Trim());
+                int[] C = { 4000, 8000, 12000 };
+                double[] K = new double[3];
+                double Lc = 100;
+                double T2 = 0;
+                HashtableList = new ArrayList();
+                while (Lc > Dc)
+                {
+                    AnswerHashTable = new Hashtable();
+                    double Ft = 0;
+                    double Zs = -(4644.7) / (1.8 * T1 + 492);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        K[i] = C[i]*Math.Pow(Math.E, Zs);
+                        Ft += K[i];
+                        AnswerHashTable.Add("K"+i, K[i]);
+                    }
+
+                    double St = Ft * (1.8 * 4644.7) / (Math.Pow(1.8 * T1 + 492, 2))/3;
+                    Lc = Ft / 3-1;
+                    T2 = T1 - (Lc / St);
+                    T1 = T2;
+                    AnswerHashTable.Add("ST", St);
+                    AnswerHashTable.Add("LC", Lc);
+                    AnswerHashTable.Add("T2", T2);
+                    HashtableList.Add(AnswerHashTable);
+                }
+                AnswerT = T2.ToString();
+                Answer.Text = AnswerT;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("*_*! 啊哦，似乎出错了！请检查数据格式哈"+ex, "Error", MessageBoxButton.OK);
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            foreach  (Hashtable item  in HashtableList)
+            {
+                //item["asd"];
+            }
+        }
+    }
+}
