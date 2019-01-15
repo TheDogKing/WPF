b26d827706f3f2ae5b3c3aa851484c6c53c88a76
DK9527 <DK-9527@outlook.com>
2019-01-15 18:17:21 +0800

    多线程

---
+++ FindT/IN_BeginIN.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FindT
+{
+    /// <summary>
+    /// IN_BeginIN.xaml 的交互逻辑
+    /// </summary>
+    public partial class IN_BeginIN : Page
+    {
+        public IN_BeginIN()
+        {
+            InitializeComponent();
+        }
+
+        private void BeginIN_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Start");
+            //int x = 0;
+            Action<int> ok = Ok;
+            for (int i=0;i<5;i++)
+            {
+                ok.BeginInvoke(i,null, null);
+            }
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + "————End");
+
+        }
+
+        private void IN_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Sync Start");
+            Action<int> ok = Ok;
+            for (int i = 0; i < 5; i++)
+            {
+                ok.Invoke(i);
+            }
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Sync End");
+        }
+
+        private void Ok(int i)
+        {
+            Console.WriteLine($"OK{i} Strat "+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+            for(int j=0;j<1000000000;j++)
+            {
+
+            }
+            Console.WriteLine($"OK{i} End " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+        }
+    }
+}
---
+++ FindT/MultiThreading.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FindT
+{
+    /// <summary>
+    /// MultiThreading.xaml 的交互逻辑
+    /// </summary>
+    public partial class MultiThreading : Page
+    {
+        public MultiThreading()
+        {
+            InitializeComponent();
+        }
+
+        //回调
+        private delegate void settextbox(string value);
+        private settextbox setitem;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            setitem = new settextbox(SetValuesss);
+            Thread ThreadT = new Thread(new ThreadStart(Newthread));
+            ThreadT.Start();
+        }
+
+        private void SetValuesss(string value)
+        {
+            this.TB.Text = value.ToString();
+        }
+
+        public void Newthread()
+        {
+            //TB.Text = DateTime.Now.ToLongTimeString()+"无参";
+            TB.Dispatcher.Invoke(setitem,DateTime.Now.ToLongDateString());
+        }
+    }
+}
