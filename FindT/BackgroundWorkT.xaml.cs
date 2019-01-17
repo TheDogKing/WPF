3c0e4d49b1f0a95236fceea5c8c5a2c86dabc577
DK9527 <DK-9527@outlook.com>
2019-01-17 17:26:21 +0800

    进度条+自动关闭

---
+++ FindT/BackgroundWorkT.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FindT
+{
+    /// <summary>
+    /// BackgroundWorkT.xaml 的交互逻辑
+    /// </summary>
+    public partial class BackgroundWorkT : Page
+    {
+        private BackgroundWorker bw = new BackgroundWorker();
+        Progress ok = new Progress();
+        Window x = new Window();
+        Window windowAdnore;
+        public BackgroundWorkT()
+        {
+            InitializeComponent();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;//支持取消
+            bw.DoWork += new DoWorkEventHandler(BGWorker_DoWork);//开始工作
+            bw.ProgressChanged += new ProgressChangedEventHandler(BgWorker_ProgessChanged);//进度改变事件
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BgWorker_WorkerCompleted);//进度完成事件
+        }
+
+        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //在这里执行耗时的运算。
+            //(string)e.Argument == "参数";
+            for (int i = 0; i <= 100; i++)
+            {
+
+                if (bw.CancellationPending)
+                {//用户取消了工作
+                    e.Cancel = true;
+                    return;
+                }
+                else
+                {
+                    bw.ReportProgress(i, "Working");//报告进度，触发ProgressChanged事件
+                    Thread.Sleep(10);//模拟工作
+                }
+            }
+        }
+        public void BgWorker_ProgessChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //(string)e.UserState=="Working"
+            ok.progressbar.Value= e.ProgressPercentage;
+            //progressbar.Value = e.ProgressPercentage;//取得进度更新控件，不用Invoke了
+        }
+
+        public void BgWorker_WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var collections = Application.Current.Windows;
+            //e.Error == null 是否发生错误
+            //e.Result
+            //x.Content = null;
+            x.Close();
+            windowAdnore.Close();
+            collections = Application.Current.Windows;
+            foreach (Window item in collections)
+            {
+                item.Close();
+                break;
+            }
+            collections = Application.Current.Windows;
+            //e.Cancelled 完成是由于取消还是正常完成
+        }
+
+        private void Test()
+        {
+            //后台工作运行中，避免重入
+            var collections = Application.Current.Windows;
+            if (bw.IsBusy) return;
+            x = new Window
+            {
+                Content = ok
+            };
+            x.Show();
+            windowAdnore = Application.Current.Windows[Application.Current.Windows.Count - 1];
+            bw.RunWorkerAsync("参数");
+        }
+
+        private void Backgroudwork_Click(object sender, RoutedEventArgs e)
+        {
+            Action testaction = Test;
+            App.Current.Dispatcher.BeginInvoke(testaction);
+        }
+    }
+}
--- FindT/IN_BeginIN.xaml.cs
+++ FindT/IN_BeginIN.xaml.cs
@@ -22,52 +22,11 @@ namespace FindT
     /// </summary>
     public partial class IN_BeginIN : Page
     {
-
-        private BackgroundWorker bw = new BackgroundWorker();
-        Progress ok = new Progress();
         public IN_BeginIN()
         {
             InitializeComponent();
-            bw.WorkerReportsProgress = true;
-            bw.WorkerSupportsCancellation = true;//支持取消
-            bw.DoWork += new DoWorkEventHandler(BGWorker_DoWork);//开始工作
-            bw.ProgressChanged += new ProgressChangedEventHandler(bgWorker_ProgessChanged);//进度改变事件
-            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_WorkerCompleted);//进度完成事件
         }
 
-
-        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            //在这里执行耗时的运算。
-            //(string)e.Argument == "参数";
-            for (int i = 0; i <= 100; i++)
-            {
-
-                if (bw.CancellationPending)
-                {//用户取消了工作
-                    e.Cancel = true;
-                    return;
-                }
-                else
-                {
-                    bw.ReportProgress(i, "Working");//报告进度，触发ProgressChanged事件
-                    Thread.Sleep(10);//模拟工作
-                }
-            }
-        }
-
-        public void bgWorker_ProgessChanged(object sender, ProgressChangedEventArgs e)
-        {
-            //(string)e.UserState=="Working"
-           ok.progressbar.Value= e.ProgressPercentage;
-            //progressbar.Value = e.ProgressPercentage;//取得进度更新控件，不用Invoke了
-        }
-
-        public void bgWorker_WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            //e.Error == null 是否发生错误
-            //e.Cancelled 完成是由于取消还是正常完成
-        }
         private void BeginIN_Button_Click(object sender, RoutedEventArgs e)
         {
             Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Start");
@@ -83,20 +42,20 @@ namespace FindT
 
         private void IN_Button_Click(object sender, RoutedEventArgs e)
         {
-            Action testaction = test;
-            App.Current.Dispatcher.BeginInvoke(testaction);
 
-            //Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Sync Start");
-            //Action<int> ok = Ok;
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    ok.Invoke(i);
-            //}
-            //Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Sync End");
+
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Sync Start");
+            Action<int> ok = Ok;
+            for (int i = 0; i < 5; i++)
+            {
+                ok.Invoke(i);
+            }
+            Console.WriteLine(DateTime.Now.ToLongTimeString() + "————Sync End");
         }
 
         private void Ok(int i)
         {
+            //var collections = Application.Current.Windows;
             Console.WriteLine($"OK{i} Strat "+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
             for(int j=0;j<1000000000;j++)
             {
@@ -105,17 +64,5 @@ namespace FindT
             Console.WriteLine($"OK{i} End " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
         }
 
-        private void test()
-        {
-            //后台工作运行中，避免重入
-            if (bw.IsBusy) return;
-            Window x = new Window
-            {
-                Content = ok
-            };
-            x.Show();
-            bw.RunWorkerAsync("参数");
-        }
-        
     }
 }
